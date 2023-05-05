63d3f1a64dc9e88d1caecd123bfcd80c16e3b6b9
snowflakeswikar <132647245+snowflakeswikar@users.noreply.github.com>
2023-05-05 15:48:10 +0530

    Update streamlit_app.py
    added picker and index creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,12 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avacado toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
