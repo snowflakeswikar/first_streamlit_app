be8d9c0783fb2100b9465bb4d6dbb93f5a636487
snowflakeswikar <132647245+snowflakeswikar@users.noreply.github.com>
2023-05-05 15:12:39 +0530

    Update streamlit_app.py
    syntax issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title('My parents new healthy diner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑'Avacado toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avacado toast')
